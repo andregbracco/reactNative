75a231341667b188b8f15b96d132e7607c52fb27
andrea <andygbracco@gmail.com>
2023-04-05 13:54:14 -0300

    estructura del login/register

--- src/screens/Account/LoginScreen/LoginScreen.js
+++ src/screens/Account/LoginScreen/LoginScreen.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Text, Image } from "@rneui/themed";
+import { screen } from "../../../utils/";
 import { styles } from "./LoginScreen.styles";
 
 export function LoginScreen() {
+  const navigation = useNavigation();
+  const goToRegister = () => {
+    navigation.navigate(screen.cuenta.register);
+  };
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <ScrollView>
+      <Image
+        source={require("../../../../assets/img/login.png")}
+        style={styles.image}
+      />
+      <View style={styles.content}>
+        <Text>Estamos en el login</Text>
+        <Text onPress={goToRegister}>Registrarse</Text>
+      </View>
+    </ScrollView>
   );
 }
